60ab975495e1b3288b7e0a6dbfe48eb3a5abb9bc
Sdg877 <sdrakegill@gmail.com>
2024-08-16 14:57:34 +0100

    requests working properly and loading bar added

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,60 +1,7 @@
-// import { useState } from 'react'
-// import './main.scss';
-// import { Stack, TextField, FormControl, InputLabel, Select, MenuItem, Button, Snackbar } from "@mui/material"
-
-// const sizes = {
-//     small: "256x256",
-//     medium: "512x512",
-//     large: "1024x1024"
-// }
-
-// function Main() {
-//     const [prompt, setPrompt ] = useState("");
-//     const [size, setSize] = useState(sizes.small)
-//     const [open, setOpen] = useState(false)
-
-//     const clickHandler = () => {
-//       console.log({ prompt, size });
-
-//       if (prompt === "") {
-//         // alert("Please enter something");
-//         setOpen(true);
-//         return;
-//       }
-//     }
-
-//   return (
-//     <div className="main">
-//         <Stack spacing={1} className="main-stack">
-//             <TextField label="Prompt" variant="outlined" />
-//             <FormControl fullWidth>
-//                <InputLabel>Size</InputLabel>
-//                <Select value={size} label="Size" onChange={(e) => setSize(e.target.value)}>
-//                   <MenuItem value={sizes.small}>small</MenuItem>
-//                   <MenuItem value={sizes.medium}>medium</MenuItem>
-//                   <MenuItem value={sizes.large}>large</MenuItem>
-//                </Select>
-//                <Button variant="contained" onClick={clickHandler} sx={{ mt: "1rem" }}>
-//                   Generate New Image
-//                </Button>
-//             </FormControl>
-//         </Stack>
-
-//         <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)} message="Note archived">
-//         {/* <Alert severity="error">This is an error alert — check it out!</Alert> */}
-//         <h5>Error</h5>
-//         </Snackbar>
-//     </div>
-//   )
-// }
-
-// export default Main
-
-
 import { useState } from 'react';
 import './main.scss';
 import axios from "axios";
-import { Stack, TextField, FormControl, InputLabel, Select, MenuItem, Button, Snackbar, Alert } from "@mui/material";
+import { Stack, TextField, FormControl, InputLabel, Select, MenuItem, Button, Snackbar, Alert, CircularProgress } from "@mui/material";
 
 const sizes = {
     small: "256x256",
@@ -66,6 +13,8 @@ function Main() {
     const [prompt, setPrompt ] = useState(""); 
     const [size, setSize] = useState(sizes.small);
     const [open, setOpen] = useState(false);
+    const [img, setImg] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const clickHandler = async () => {
       try {
@@ -74,12 +23,17 @@ function Main() {
           setOpen(true);
           return;
         }
+        setLoading(true);
         console.log({ prompt, size }); 
-        const url = "http://localhost8200/generate";
+        const url = "http://localhost:8200/generate";
         const data ={ prompt, size };
         const response = await axios.post(url, data);
+        const imgSrc = response.data.src;
+        setImg(imgSrc)
+        setLoading(false);
       } catch (error) {
-
+        setOpen(true);
+        setLoading(false);
       }
     }
 
@@ -104,6 +58,8 @@ function Main() {
                  </Button>
               </FormControl>
           </Stack>
+          {loading && <CircularProgress color="success" />}
+          {img !== "" && <img src={img} alt="img" />}
 
           <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)} message="Please enter something">
           <Alert severity="error">This is an error alert — check it out!</Alert>
